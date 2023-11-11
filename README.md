148328b6d73bace8e6d49b05934acd288cf55f0b
scenaristeur <scenaristeur@gmail.com>
2023-11-11 15:58:49 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# objectif lancer un son ou une video au passage devant un capteur HC-SR04
+
+
+# install
+` pip install -r requirements.txt`
+
+- librairies pymata4 https://mryslab.github.io/pymata4/install_pymata4/
+- avec firmataExpress https://mryslab.github.io/pymata4/firmata_express/
+
+
+# Tuto video
+- https://www.youtube.com/watch?v=c1ttso5tYgA
+
+
+# vlc player
+- https://stackoverflow.com/questions/44472162/how-can-i-play-audio-playsound-in-the-background-of-a-python-script
---
+++ lumos.py
@@ -0,0 +1,46 @@
+import time
+from pymata4 import pymata4
+import os 
+import vlc
+
+count=0
+trigpin=11
+ecopin=12
+
+plouf_Is_reading = False
+
+plouf_player = vlc.MediaPlayer("plouf.mp3")
+
+board = pymata4.Pymata4()
+
+# def readPlouf():
+#     playsound.playsound("plouf.mp3", True)
+
+
+def callbackSR04_1(data):
+    global count
+    print("distance ", data[2])
+    if data[2] < 20 :
+        plouf_player.play()
+    else:
+        plouf_player.pause()
+        plouf_player.stop()
+
+        
+
+   
+
+
+
+board.set_pin_mode_sonar(trigpin, ecopin, callbackSR04_1)
+#count=0
+while True:
+    try: 
+        time.sleep(0.1)
+        print("is reading",plouf_Is_reading)
+        if plouf_Is_reading == False :
+            print("read")
+            board.sonar_read(trigpin)
+    except Exception:
+        board.shutdown()
+
